60e7b874615f9c7216387871e0668c956caa1b5b
akshay07k <akshaykumar5651t@gmail.com>
2024-01-27 23:44:40 +0530

    Major Project

---
+++ 07MegaBlog/src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import './App.css'
+import authServices from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authServices.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap 
+    content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
+}
+
+export default App
---
+++ 07MegaBlog/src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './container/Container'
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button';
+import Login from "./Login";
+import Input from './Input';
+import RTE from "./RTE";
+import AuthLayout from './AuthLayout'
+import PostCard from './PostCard'
+import Select from './Select'
+import Signup from './Signup'
+import PostForm from './post-form/PostForm'
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    Button,
+    Login,
+    Input,
+    RTE,
+    AuthLayout,
+    PostCard,
+    Select,
+    Signup,
+    PostForm
+}
